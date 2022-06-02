9cb2033b3fa077f7b7211d43d3667bc277bb3145
Orf1 <orfeas126@gmail.com>
2022-06-02 13:54:42 -0700

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Orfeas Magoulas on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
